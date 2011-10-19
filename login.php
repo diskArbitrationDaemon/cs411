7a5431525fbed1cd3e4805a37ec42b66f92e231d
Chengzhe Wu <chengzhe.wu@gmail.com>
2011-10-19 15:17:21 -0500

    added login header. now login page handles fails. Not done anything about login success yet!

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+    include 'login_header.php';
     $mySqlHost = "localhost";
     $mySqlUser = "login_auth";
     $mySqlPass = "hello123";
@@ -15,13 +16,30 @@
 
     mysql_select_db($loginDb) or die("Can not connect to login table.");
 
-    $login_query = "SELECT * FROM " . $loginTable . " WHERE Username='$username' AND Password='$password'";
+    $login_query = "SELECT * FROM " . $loginTable . " WHERE Username='$username' AND Password='$encryptedPassword'";
+    print($login_query);
     $login_result = mysql_query($login_query);
-    $rows = mysql_num_rows($login_result); 
+    $rows = mysql_num_rows($login_result);
+    $referrer = $_SERVER["HTTP_REFERER"];
+    $referrer = substr($_SERVER["HTTP_REFERER"], 0,  (strpos($_SERVER["HTTP_REFERER"], "?")));
+    //remove any GET values
+
     if (!$rows){
-        die ("Login error");
+        //send user back to login page
+        print("<html> <head>");
+        print("<meta http-equiv=\"REFRESH\" content=\"3;url=". $referrer . "?fail=1\"></HEAD>");
+        print("<BODY></BODY></HTML>");
+    } else {
+        $_SESSION['username'] = $username;
+        $login_row = mysql_fetch_array($login_result);
+        $userType = $login_row['UserType'];
+        
+        print("userType = " . $userType);
+        print("<br>");
+
+        //depending on which type of user (instructor, admin, student)
     }
-    print ($login_result."<br>"."rows: ".$rows."<br>");
-    print ("LOGIN SUCCESS!");
+
+
 
 ?>
---
+++ login_header.php
@@ -0,0 +1,5 @@
+<?php
+    define("ADMINISTRATOR", 1);
+    define("INSTRUCTOR", 2);
+    define("STUDENT", 3);
+?>
