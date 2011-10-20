7c4bd5d8da33889ba1b13d1bf35ffdd2cb0f296c
Chengzhe Wu <chengzhe.wu@gmail.com>
2011-10-20 18:29:24 -0500

    changed login script to reflect header files are now int "includes" and simplified the student, admin and instructor pages

--- login.php
+++ login.php
@@ -1,8 +1,11 @@
 <?php
-    include 'login_header.php';
+    include 'includes/login_header.php';
+    print("<html> <head></head><body>");
+    
     $mySqlHost = "localhost";
     $mySqlUser = "login_auth";
     $mySqlPass = "hello123";
+    $redirect_pauseTime = 3;
 
     $mysqlConnection = mysql_connect($mySqlHost, $mySqlUser, $mySqlPass) or die("Can not connect to DB. " . mysql_error());
     
@@ -15,31 +18,42 @@
     $loginTable = "users";
 
     mysql_select_db($loginDb) or die("Can not connect to login table.");
-
+   
     $login_query = "SELECT * FROM " . $loginTable . " WHERE Username='$username' AND Password='$encryptedPassword'";
-    print($login_query);
+    //print($login_query);
     $login_result = mysql_query($login_query);
     $rows = mysql_num_rows($login_result);
     $referrer = $_SERVER["HTTP_REFERER"];
-    $referrer = substr($_SERVER["HTTP_REFERER"], 0,  (strpos($_SERVER["HTTP_REFERER"], "?")));
     //remove any GET values
 
+    print("rows: $rows");
     if (!$rows){
         //send user back to login page
-        print("<html> <head>");
-        print("<meta http-equiv=\"REFRESH\" content=\"3;url=". $referrer . "?fail=1\"></HEAD>");
-        print("<BODY></BODY></HTML>");
+        print("<meta http-equiv=\"REFRESH\" content=\"". $redirect_pauseTime . ";url=login.html?fail=1\">");
     } else {
         $_SESSION['username'] = $username;
         $login_row = mysql_fetch_array($login_result);
         $userType = $login_row['UserType'];
-        
+    
+        //different users will have different redirect pagesi
+        if ($userType == ADMINISTRATOR){
+            $url = ADMIN_PHP;
+        } else if ($userType == INSTRUCTOR){
+            $url = INSTRUCTOR_PHPi;
+        } else if ($userType == STUDENT){
+            $url = STUDENT_PHP;
+        } else {
+            die("User type not found");
+        }
+    
         print("userType = " . $userType);
         print("<br>");
 
-        //depending on which type of user (instructor, admin, student)
+        print("<meta http-equiv=\"REFRESH\" content=\"" . $redirect_pauseTime ." ;url=" . $url . "\">");
+       
     }
 
-
+    
+        print("</BODY></HTML>");
 
 ?>
