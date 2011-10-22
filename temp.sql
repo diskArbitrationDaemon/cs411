8d6b5378f6466d8e41fb3734e33e4a0a6bdf804b
Chengzhe Wu <chengzhe.wu@gmail.com>
2011-10-21 19:27:37 -0500

    added foreign key constraints

--- temp.sql
+++ temp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2011 at 06:30 PM
+-- Generation Time: Oct 21, 2011 at 07:26 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.4
 
@@ -33,10 +33,19 @@ CREATE TABLE IF NOT EXISTS `Assignment` (
   `MaxMark` int(3) DEFAULT NULL,
   `AvgMark` double DEFAULT NULL,
   `MedianMark` double DEFAULT NULL,
-  `CourseID` varchar(10) DEFAULT NULL,
+  `CourseID` int(10) DEFAULT NULL,
   `DueTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`AssnID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`AssnID`),
+  KEY `CourseID` (`CourseID`),
+  KEY `CourseID_2` (`CourseID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `Assignment`
+--
+
+INSERT INTO `Assignment` (`AssnID`, `AssnName`, `GroupWork`, `MaxMark`, `AvgMark`, `MedianMark`, `CourseID`, `DueTime`) VALUES
+(1, 'Assignment1', 0, 100, 0, 0, 1, '2011-10-26 05:00:00');
 
 -- --------------------------------------------------------
 
@@ -49,7 +58,8 @@ CREATE TABLE IF NOT EXISTS `Automarking` (
   `SampleSoln` varchar(200) NOT NULL COMMENT 'Directory in which the sample solution is stored',
   `Configs` text NOT NULL,
   `AssnID` int(30) NOT NULL,
-  PRIMARY KEY (`AutomarkID`)
+  PRIMARY KEY (`AutomarkID`),
+  KEY `AssnID` (`AssnID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -64,7 +74,14 @@ CREATE TABLE IF NOT EXISTS `Course` (
   `NumStudents` int(4) NOT NULL,
   `SemesterName` varchar(6) NOT NULL,
   PRIMARY KEY (`CourseID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `Course`
+--
+
+INSERT INTO `Course` (`CourseID`, `CourseName`, `NumStudents`, `SemesterName`) VALUES
+(1, 'CS411', 60, 'Fall 1');
 
 -- --------------------------------------------------------
 
@@ -76,7 +93,9 @@ CREATE TABLE IF NOT EXISTS `Group` (
   `GroupName` varchar(100) NOT NULL,
   `CourseID` int(10) NOT NULL,
   `AssnID` int(30) NOT NULL,
-  PRIMARY KEY (`GroupName`,`CourseID`,`AssnID`)
+  PRIMARY KEY (`GroupName`,`CourseID`,`AssnID`),
+  KEY `CourseID` (`CourseID`),
+  KEY `AssnID` (`AssnID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -105,7 +124,10 @@ CREATE TABLE IF NOT EXISTS `MemberOf` (
   `StudentID` varchar(20) NOT NULL,
   `AssnID` int(30) NOT NULL,
   `CourseID` int(10) NOT NULL,
-  PRIMARY KEY (`GroupName`,`StudentID`,`AssnID`,`CourseID`)
+  PRIMARY KEY (`GroupName`,`StudentID`,`AssnID`,`CourseID`),
+  KEY `StudentID` (`StudentID`),
+  KEY `AssnID` (`AssnID`),
+  KEY `CourseID` (`CourseID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -135,7 +157,9 @@ CREATE TABLE IF NOT EXISTS `Submission` (
   `Files` varchar(200) NOT NULL COMMENT 'The directory where the submission files are locatedt',
   `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `AssnMark` int(3) NOT NULL,
-  PRIMARY KEY (`AssnID`,`CourseID`,`Student`)
+  PRIMARY KEY (`AssnID`,`CourseID`,`Student`),
+  KEY `CourseID` (`CourseID`),
+  KEY `Student` (`Student`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -148,7 +172,8 @@ CREATE TABLE IF NOT EXISTS `Takes` (
   `Student` varchar(20) NOT NULL,
   `Course` int(10) NOT NULL,
   `FinalMark` int(3) NOT NULL,
-  PRIMARY KEY (`Student`,`Course`)
+  PRIMARY KEY (`Student`,`Course`),
+  KEY `Course` (`Course`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -160,8 +185,61 @@ CREATE TABLE IF NOT EXISTS `Takes` (
 CREATE TABLE IF NOT EXISTS `Teaches` (
   `Instructor` varchar(10) NOT NULL,
   `Course` int(10) NOT NULL,
-  PRIMARY KEY (`Instructor`,`Course`)
+  PRIMARY KEY (`Instructor`,`Course`),
+  KEY `Course` (`Course`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Assignment`
+--
+ALTER TABLE `Assignment`
+  ADD CONSTRAINT `assignment_ibfk_1` FOREIGN KEY (`CourseID`) REFERENCES `Course` (`CourseID`);
+
+--
+-- Constraints for table `Automarking`
+--
+ALTER TABLE `Automarking`
+  ADD CONSTRAINT `automarking_ibfk_1` FOREIGN KEY (`AssnID`) REFERENCES `Assignment` (`AssnID`);
+
+--
+-- Constraints for table `Group`
+--
+ALTER TABLE `Group`
+  ADD CONSTRAINT `group_ibfk_2` FOREIGN KEY (`AssnID`) REFERENCES `Assignment` (`AssnID`),
+  ADD CONSTRAINT `group_ibfk_1` FOREIGN KEY (`CourseID`) REFERENCES `Assignment` (`CourseID`);
+
+--
+-- Constraints for table `MemberOf`
+--
+ALTER TABLE `MemberOf`
+  ADD CONSTRAINT `memberof_ibfk_3` FOREIGN KEY (`CourseID`) REFERENCES `Assignment` (`CourseID`),
+  ADD CONSTRAINT `memberof_ibfk_1` FOREIGN KEY (`StudentID`) REFERENCES `Student` (`StudentID`),
+  ADD CONSTRAINT `memberof_ibfk_2` FOREIGN KEY (`AssnID`) REFERENCES `Assignment` (`AssnID`);
+
+--
+-- Constraints for table `Submission`
+--
+ALTER TABLE `Submission`
+  ADD CONSTRAINT `submission_ibfk_2` FOREIGN KEY (`Student`) REFERENCES `Student` (`StudentID`),
+  ADD CONSTRAINT `submission_ibfk_1` FOREIGN KEY (`CourseID`) REFERENCES `Assignment` (`CourseID`);
+
+--
+-- Constraints for table `Takes`
+--
+ALTER TABLE `Takes`
+  ADD CONSTRAINT `takes_ibfk_2` FOREIGN KEY (`Course`) REFERENCES `Course` (`CourseID`),
+  ADD CONSTRAINT `takes_ibfk_1` FOREIGN KEY (`Student`) REFERENCES `Student` (`StudentID`);
+
+--
+-- Constraints for table `Teaches`
+--
+ALTER TABLE `Teaches`
+  ADD CONSTRAINT `teaches_ibfk_2` FOREIGN KEY (`Course`) REFERENCES `Assignment` (`CourseID`),
+  ADD CONSTRAINT `teaches_ibfk_1` FOREIGN KEY (`Instructor`) REFERENCES `Instructor` (`NetID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
