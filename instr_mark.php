300cc11b70c4a26604664242b3acb5d00d133b51
Chengzhe Wu <chengzhe.wu@gmail.com>
2011-11-06 22:51:24 -0600

    fixed a mysql query bug where it assumes that there is always an existing entry in the results table.

--- instr_mark.php
+++ instr_mark.php
@@ -19,23 +19,32 @@ if (empty($stage)){
 
         $row = mysql_fetch_array($result);
         //get questions
-        $query = "SELECT QuestionName, Questions.QuestionID, Mark FROM Questions, Assignment, Course, Takes, Results WHERE
+        $query = "SELECT QuestionName, Questions.QuestionID FROM Questions, Assignment, Course, Takes WHERE
         Assignment.AssnID='". htmlspecialchars($_GET['AssnID']) ."' AND
         Takes.CourseID = Course.CourseID AND
         Takes.StudentID = '".htmlspecialchars($_GET['StudentID'])."' AND
         Assignment.CourseID = Course.CourseID AND
-        Questions.AssnID = Assignment.AssnID AND
-        Results.QuestionID = Questions.QuestionID";
+        Questions.AssnID = Assignment.AssnID"; 
+
+
         $result = mysql_query($query);
         if (mysql_errno()) print(mysql_error());
         if (mysql_num_rows($result) == 0){
-            die ("this assignment/student combination does not exist");
+            print ("There are no questions for this assignment");
         } else {
             print("<form name=form action=instr_mark.html  method=\"post\">\n");
             print("<table border=0 width=400>\n");
             $i = 0;
             while ($row = mysql_fetch_array($result)){
-                print("<tr><td >" . $row['QuestionName'] . "</td><td><input type=text name=Q". $row['QuestionID'] ." length=5 value=\"".$row['Mark']."\"></td></tr>\n");
+
+                $query = "SELECT Mark FROM Results WHERE StudentID='".$_GET['StudentID']."' AND QuestionID='".$row['QuestionID']."'";
+                $markQueryResult = mysql_query($query);
+                $mark = "";
+                if (mysql_num_rows($markQueryResult)){
+                    $markRow = mysql_fetch_array($markQueryResult);
+                    $mark = $markRow['Mark'];
+                }
+                print("<tr><td >" . $row['QuestionName'] . "</td><td><input type=text name=Q". $row['QuestionID'] ." length=5 value=\"".$mark."\"></td></tr>\n");
                 print("<input type=hidden name=Question$i value=".$row['QuestionID'].">\n<br>");
                 $i++;
             }
@@ -56,7 +65,8 @@ if (empty($stage)){
     $studentID = $_POST['StudentID'];
     $assnID = $_POST['AssnID'];
     foreach ($_POST['Question'] as $key => $value){ 
-        mysql_query("INSERT into Results VALUES ('$studentID', '$key', '$value')
+
+        if (!empty($value)) mysql_query("INSERT into Results VALUES ('$studentID', '$key', '$value')
 
         ON DUPLICATE KEY UPDATE mark='$value'");
         if (mysql_errno() > 0) print("error: " . mysql_error() . "<br> Query: " . $query);
