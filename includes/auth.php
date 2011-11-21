80e1f7fefc6fd1e0804e538dfdecd763650f24ad
Chengzhe Wu <chengzhe.wu@gmail.com>
2011-11-20 22:25:37 -0600

    There's a bug where the current configuration does not check if a student can actually see what the instructor sees by typing in the URL. I've placed a manual check in there. Please see the diffs for my changes!

--- includes/auth.php
+++ includes/auth.php
@@ -1,5 +1,4 @@
 <?php
-    session_start();
 $redirect_pauseTime = 0;
 if(empty($_SESSION['username'])) 
         print("<meta http-equiv=\"REFRESH\" content=\"". $redirect_pauseTime . ";url=login.html?notLoggedIn=1\">");
--- includes/mysqlInstrLogin.php
+++ includes/mysqlInstrLogin.php
@@ -1,5 +1,26 @@
 <?php
+    	session_start();
 
+
+        $mySqlHost = "localhost";
+        $mySqlUser = "login_auth";
+        $mySqlPass = "hello123";
+
+        $mysqlConnection = mysql_connect($mySqlHost, $mySqlUser, $mySqlPass) or die("Can not connect to DB. " . mysql_error());
+        mysql_select_db("assignments_users_uiuc");
+        if (mysql_errno()) die ("ERROR: ".mysql_error());
+        
+        $query = "SELECT UserType FROM users WHERE username ='$_SESSION[username]'";
+		$result = mysql_query($query);
+		if (mysql_errno()) die ("ERROR: ".mysql_error());
+        $row = mysql_fetch_array($result);
+		$userType = $row['UserType'];
+		
+		if ($userType != 2){
+			die ("Not instructor. Permission denied. Please notify the database administrator
+			of this error.");
+		}
+		
         $mySqlHost = "localhost";
         $mySqlUser = "instructor";
         $mySqlPass = "hello123";
@@ -10,4 +31,6 @@
         $courseTable = "Course";
         $assignmentTable = "Assignment";
         $instructorTable = "Instructor";
+        
+        
 ?>
