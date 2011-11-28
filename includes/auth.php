fa876d3452cc488f3078ba0b0aae17f30121f3be
Chengzhe Wu <chengzhe.wu@gmail.com>
2011-11-28 00:20:38 -0600

    fixed the error message once and for all`

--- includes/auth.php
+++ includes/auth.php
@@ -1,6 +1,11 @@
 <?php
+if (empty($_SESSION['username']) && !isset ($_COOKIE[ini_get('session.name')])) {
+    session_start();
+}
 $redirect_pauseTime = 0;
-if(empty($_SESSION['username'])) 
-        print("<meta http-equiv=\"REFRESH\" content=\"". $redirect_pauseTime . ";url=login.html?notLoggedIn=1\">");
-
+if(empty($_SESSION['username'])) {
+	print("Not logged in. Please click <a href=\"login.html\">here</a> to log in.");
+	die();
+}
+	
 ?>
