987d2dc980596fe0adb45729fa99e246d845c722
Chengzhe Wu <chengzhe.wu@gmail.com>
2011-11-28 00:35:22 -0600

    new style of auth. now should work.

--- includes/mysqlstudentLogin.php
+++ includes/mysqlstudentLogin.php
@@ -1,13 +1,17 @@
 <?php
+    	session_start();
+		if (isset ($_SESSION['username'])){
 
-        $mySqlHost = "localhost";
-        $mySqlUser = "student";
-        $mySqlPass = "hello123";
-
-        $mysqlConnection = mysql_connect($mySqlHost, $mySqlUser, $mySqlPass) or die("Can not connect to DB. " . mysql_error());
-        mysql_select_db("assignments_uiuc");
-
-        $courseTable = "course";
-        $assignmentTable = "assignment";
-        $instructorTable = "instructor";
+	        $mySqlHost = "localhost";
+	        $mySqlUser = "student";
+	        $mySqlPass = "hello123";
+	
+	        $mysqlConnection = mysql_connect($mySqlHost, $mySqlUser, $mySqlPass) or die("Can not connect to DB. " . mysql_error());
+	        mysql_select_db("assignments_uiuc");
+	
+	        $courseTable = "course";
+	        $assignmentTable = "assignment";
+	        $instructorTable = "instructor";
+        
+		}
 ?>
